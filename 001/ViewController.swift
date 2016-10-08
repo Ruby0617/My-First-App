dbf3d32e502932642050318d462443204fd3151a
Ruby Chang <Apple@apples-macbook-pro.local>
2016-10-08 11:53:43 +0800

    Initial Commit

---
+++ 001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  001
+//
+//  Created by Ruby Chang on 2016/10/8.
+//  Copyright © 2016年 Ruby Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
